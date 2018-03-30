f53acd5417d758e730844f6ff15459277f03dae3
Quentin <quent.guillaume@gmail.com>
2018-03-30 14:11:38 -0800

    Hashes 1

--- 04-hashes-part-1/hashclass.rb
+++ 04-hashes-part-1/hashclass.rb
@@ -1,3 +1,5 @@
+require_relative 'hash_item'
+
 class HashClass
 
   def initialize(size)
@@ -8,25 +10,45 @@ class HashClass
     new_value = HashItem.new(key, value)
     i = index(key, size)
 
-    
+    if @items[i] === nil
+      @items[i] = new_value
+    elsif @items[i].key != key
+      self.resize
+      self[key] = value
+    elsif (@items[i].key === key) && (@items[i].value != value)
+      self.resize
+      @items[i] = value
+    end
   end
 
 
   def [](key)
+    if @items[index(key,size)] != nil && @items[index(key,size)].key == key
+      @items[index(key,size)].value
+    end
   end
 
   def resize
+  arrayb = @items
+    @items = Array.new(arrayb.length * 2)
+
+    arrayb.each do |item|
+      unless item === nil
+        self[item.key] = item.value
+      end
+    end
   end
 
-  # Returns a unique, deterministically reproducible index into an array
-  # We are hashing based on strings, let's use the ascii value of each string as
-  # a starting point.
   def index(key, size)
-
+    result = 0
+    key.each_byte do |letter|
+    result += letter
+  end
+  result % size
   end
 
-  # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
 end
